0529aa68dd799a94ca2c4fe2a5b19f92e721c93c
Ben-JT <tintin7311@gmail.com>
2024-11-21 15:07:59 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Nov 21, 2024 02:18:',
+  birthdate: 'Nov 21, 2024 13:09:',
   name: ' '
 };
 
